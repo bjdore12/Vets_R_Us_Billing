ae762b487fc09244c06eda6bdb1bcf127818b6b0
bjdore12 <45237685+bjdore12@users.noreply.github.com>
2020-09-04 19:27:25 -0400

    Initial commit

---
+++ src/Pet.java
@@ -0,0 +1,53 @@
+/*
+
+    Author: Benjamin J. Dore
+    Date:   9/4/2020
+
+    Description:    Pet class is used to define characteristics of the visiting pet
+                    (i.e. pet name, species, and weight.
+
+*/
+
+public class Pet {
+
+    private String petName;
+    private String petSpecies;
+    private int petWeight;          // TODO: may need to be a double
+
+    public Pet(String petName, String petSpecies, int petWeight) {
+        this.petName = petName;
+        this.petSpecies = petSpecies;
+        this.petWeight = petWeight;
+    }
+
+    public void setPetName(String newPetName) {
+        this.petName = newPetName;
+    }
+
+    public void setPetSpecies(String newPetSpecies) {
+        this.petSpecies = newPetSpecies;
+    }
+
+    public void setPetWeight(int newPetWeight) {
+        this.petWeight = newPetWeight;
+    }
+
+    public String getPetName() {
+        return this.petName;
+    }
+
+    public String getPetSpecies() {
+        return this.petSpecies;
+    }
+
+    public int getPetWeight() {
+        return this.petWeight;
+    }
+
+    @Override
+    public String toString() {
+        return "Pet Name: " + this.petName + "\n" +
+                "Pet Species: " + this.petSpecies + "\n" +
+                "Pet Weight: " + this.petWeight;
+    }
+}
---
+++ src/PetOwner.java
@@ -0,0 +1,65 @@
+/*
+
+    Author: Benjamin J. Dore
+    Date:   9/4/2020
+
+    Description:    Pet owner class is used to define a pet owners characteristics
+                    (i.e. name and phone number).
+
+*/
+
+public class PetOwner {
+
+    private String firstName;
+    private String middleInitial;
+    private String lastName;
+    private String phoneNumber;
+
+    public PetOwner(String firstName, String middleInitial, String lastName, String phoneNumber) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setFirstName(String newFirstName) {
+        this.firstName = newFirstName;
+    }
+
+    public void setMiddleInitial(String newMiddleInitial) {
+        this.middleInitial = newMiddleInitial;
+    }
+
+    public void setLastName(String newLastName) {
+        this.lastName = newLastName;
+    }
+
+    public void setPhoneNumber(String newPhoneNumber) {
+        this.phoneNumber = newPhoneNumber;
+    }
+
+    public String getFullName() {
+        return this.firstName + " " + this.middleInitial + " " + this.lastName;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getMiddleInitial() {
+        return this.middleInitial;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return this.firstName + " " + this.middleInitial + " " + this.lastName + "\n" + "Phone: " + this.phoneNumber;
+    }
+}
